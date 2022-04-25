92a789f4315837d2d286ff715b4e141e5446a16f
Luis Moura <luismoura2005@gmail.com>
2022-04-25 12:04:25 -0300

    modidificado tabela dos carros

--- cacho-pesso.py
+++ cacho-pesso.py
@@ -4,5 +4,5 @@ idade_dog = float(input('Qual a idade do seu cachorro? '))
 
 idade_hum = 76.6*idade_dog/11.5
 
-print(f'Seu cachorro tem {idade_hum} anos humanos')
+print(f'Seu cachorro tem {idade_hum:.2f} anos humanos')
 
